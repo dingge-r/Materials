b31c677372b8c7eeaee778c041ed040750a53eb0
DingGe520R <784872490@qq.com>
2020-02-20 18:45:12 +0800

    first

--- src/main/java/com/example/materials/controller/UserController.java
+++ src/main/java/com/example/materials/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户登录
+ * 用户登录xxxxxxx
  */
 @RestController
 @RequestMapping("/api/user")
